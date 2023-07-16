1919c3c96042a8685b0c22111afe96a16986005e
bilal-dallali <dallali-bilal@hotmail.fr>
2023-07-16 20:26:17 +0200

    adding photos

--- Hichat/Views/ContentView.swift
+++ Hichat/Views/ContentView.swift
@@ -9,13 +9,40 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
+        VStack(spacing: 0) {
+            
+            VStack {
+                Image("avatar-1")
+                    .resizable()
+                    .frame(width: 60, height: 60)
+                    .cornerRadius(.infinity)
+                    .rotationEffect(.degrees(-20.91))
+                    .offset(x: -195, y: -194)
+                Image("avatar-2")
+                    .resizable()
+                    .frame(width: 100, height: 100)
+                    //.cornerRadius(.infinity)
+                    //.rotationEffect(.degrees(-14.43))
+                    .offset(x: -74, y: -382)
+                Image("avatar-1")
+                    .resizable()
+                    .frame(width: 120, height: 120)
+                    //.cornerRadius(.infinity)
+                    //.rotationEffect(.degrees(-14.43))
+                    .offset(x: 108, y: -491)
+            }
+            .padding(.top, 400)
+                
             Text("Welcome to Hichat!")
                 .foregroundColor(Color("Primary500"))
                 .font(.custom("Urbanist-Bold", size: 40))
+            
         }
-        .padding(.horizontal, 24)
+        //.padding(.horizontal, 24)
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
         .background(Color("Dark1"))
+        
+        
     }
 }
 
